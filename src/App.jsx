063588ce5025076eb49ebab55d3597d0775d1e58
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-05-03 00:42:08 -0700

    Chore: structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
+import FormSubmission from './pages/FormSubmission'
 import Home from './pages/Home'
 
 function App() {
-
-  return (
-    <>
-      <Home />
-    </>
-  )
+	return (
+		<>
+			<FormSubmission />
+		</>
+	)
 }
 
 export default App
---
+++ src/pages/FormSubmission/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './styles.scss'
+
+function FormSubmission() {
+	return <></>
+}
+
+export default FormSubmission
